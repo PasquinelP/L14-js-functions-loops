9e9f788c41266cd3cc7909b902e28469dd776a80
Pascal <41127800+PasquinelP@users.noreply.github.com>
2021-02-04 15:10:36 +0100

    Add files via upload

---
+++ opdracht-function-basics/script.js
@@ -0,0 +1,31 @@
+console.log("working!");
+
+let makePizza = function () {
+  console.log("Make dough");
+  console.log("Preheat the oven");
+  console.log("Make toppings");
+  console.log("Roll pizza bottom");
+  console.log("Put on toppings");
+  console.log("Put pizza in the oven");
+  console.log("Take pizza out of oven when done");
+  console.log("Enjoy pizza");
+};
+
+let makeSushi = function() {
+  console.log("Cook sushi rice");
+  console.log("Prepare ingredients");
+  console.log("Drain rice");
+  console.log("prepare rolling mat");
+  console.log("add rice and ingredients");
+  console.log("Roll up sushi");
+  console.log("Cut sushi rolls in parts");
+  console.log("Enjoy sushi");
+}
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
